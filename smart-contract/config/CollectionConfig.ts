60eb0e874b2c6c4ba00454dc2ef9379755856b5d
T-Ph525 <95352755+T-Ph525@users.noreply.github.com>
2022-06-26 16:46:02 +1000

    Update CollectionConfig.ts

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,26 +9,26 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'AxoGenesis',
-  tokenName: 'AxoGenesis',
-  tokenSymbol: 'AxG',
+  contractName: 'axoGenesis',
+  tokenName: 'axoGenesis',
+  tokenSymbol: 'axG',
   hiddenMetadataUri: 'ipfs://process.env.COLLECTION_URI_PREFIX',
   maxSupply: 3333,
   whitelistSale: {
-    price: 50,
+    price: 0,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 50,
+    price: 0,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 50,
-    maxMintAmountPerTx: 5,
+    price: 0,
+    maxMintAmountPerTx: 3,
   },
   contractAddress: "0x162A995868546acC36fe2a6038B081CFfD87c5E0",
   marketplaceIdentifier: 'AxoGenesis',
-  marketplaceConfig: Marketplaces.openSea,
+  marketplaceConfig: Marketplaces.rarible,
   whitelistAddresses,
 };
 
